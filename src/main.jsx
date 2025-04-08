4739407a68f281d5cf703cbd0994bf61ba68dce6
Adewale Stephen <adewalestephennifemi@gmail.com>
2025-04-08 22:53:12 +0100

    Browser Router updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
 import Layout from "./Layout.jsx";
 import Home from "./Pages/Home.jsx";
 import Services from "./Pages/Services.jsx";
 import Error from "./Pages/Error.jsx";
+import About from "./Pages/About.jsx";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +22,10 @@ const router = createBrowserRouter([
       {
         path: "/services",
         element: <Services />,
+      },
+      {
+        path: "/about",
+        element: <About />,
       },
     ],
   },
